de41d097b89d35569fe0f4848779875cd8e742ba
mrznap <pedro.vidales@v2smx.com>
2025-04-08 23:44:37 -0600

    Cucumber added to test-runner

--- test-runner-specs.sh
+++ test-runner-specs.sh
@@ -1,5 +1,8 @@
-// Run user test cases
+# Run user test cases
 # npx playwright test src/specs/pet.store.user.spec.ts --project=chromium
 
-// Run pet test cases
-npx playwright test src/specs/pet.store.pets.spec.ts --project=chromium
+# Run pet test cases
+# npx playwright test src/specs/pet.store.pets.spec.ts --project=chromium
+
+# Run all test cases with cucumber
+npm run test:api
